507069d4935af66f70b925c2a4abc8c16a0f4c74
Madhuri Patil <madhuri@thoughtworks.com>
2016-02-11 08:17:57 +0530

    the horizonral bar chart for juice data is made

--- juice.js
+++ juice.js
@@ -2,20 +2,38 @@ $(document).ready(function(){
 	d3.json('./juice_orders',function(err,data){
 		if(err) console.log('err is ',err);
 		var juice =d3.nest()
-			.key(function(d){return d.drinkName})
+			.key(function(d){
+				return d.drinkName
+			})
 			.rollup(function(v) {
 				return d3.sum(v, function(d) { return d.quantity; })
 			})
 			.entries(data);
-			console.log(juice);
+			juice = juice.filter(function(d){return d.key != 'CTL' && d.key != 'Register User' && d.key != 'Fruits' && d.key != 'ctl'})
 		visualiseIt(juice);
 	});
 });
 
 
-var visualiseIt = function(){
-
+var visualiseIt = function(juices){
+	var width = 9000;
+	var height = 1000;
+	var barHeight = 20;
+	var body = d3.select('body');
+	body.append('svg')
+		.attr('class','bar_chart')
+		
+	d3.select('.bar_chart')
+		.data(juices)
+		.enter()
+		.append('div')
+		.style('width',function(d){return d.values+'px'})
+		.style('height',barHeight+'px')
+		.style('background-color','gray')
+		.style('margin','1px')
+		.text(function(d){return d.key});
+	var bar = d3.selectAll('div');
+	var chart = d3.select('.bar_chart')
+				.attr('height',height)
+				.attr('width',width);
 };
-
-
-
